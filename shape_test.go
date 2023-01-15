a03b76627fabbcaec0ba00fd324bc9c2e2a0799c
rym49 <hatanakaryoma.130@gmail.com>
2023-01-15 21:48:07 +0900

    init

---
+++ shape_test.go
@@ -0,0 +1,39 @@
+package main
+
+import "testing"
+
+func TestPerimeter(t *testing.T) {
+	rectangle := Rectangle{10.0, 10.0}
+
+	got := Perimeter(rectangle)
+	want := 40.0
+
+	if got != want {
+		t.Errorf("got %2.f, want %2.f", got, want)
+	}
+}
+
+func TestArea(t *testing.T) {
+	checkArea := func(t *testing.T, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+		if got != want {
+			t.Errorf("got %g, want %g", got, want)
+		}
+	}
+
+	t.Run("rectangle", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 10.0}
+		want := 100.0
+
+		checkArea(t, rectangle, want)
+	})
+
+	t.Run("circle", func(t *testing.T) {
+		circle := Circle{10.0}
+		want := 314.1592653589793
+
+		checkArea(t, circle, want)
+	})
+
+}
