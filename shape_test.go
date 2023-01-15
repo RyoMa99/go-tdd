95c41e6c0690609842fe8f3048b453295d69c56a
rym49 <hatanakaryoma.130@gmail.com>
2023-01-15 21:52:54 +0900

    テーブル駆動テストに変更

--- shape_test.go
+++ shape_test.go
@@ -14,26 +14,19 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	checkArea := func(t *testing.T, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %g, want %g", got, want)
-		}
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{10.0, 10.0}, 100.0},
+		{Circle{10.0}, 314.1592653589793},
 	}
 
-	t.Run("rectangle", func(t *testing.T) {
-		rectangle := Rectangle{10.0, 10.0}
-		want := 100.0
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
 
-		checkArea(t, rectangle, want)
-	})
-
-	t.Run("circle", func(t *testing.T) {
-		circle := Circle{10.0}
-		want := 314.1592653589793
-
-		checkArea(t, circle, want)
-	})
-
+		if got != tt.want {
+			t.Errorf("got %g, want %g", got, tt.want)
+		}
+	}
 }
