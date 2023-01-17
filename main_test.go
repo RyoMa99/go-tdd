37aaeb1352368ce388704fb4179253089da688e0
rym49 <hatanakaryoma.130@gmail.com>
2023-01-18 03:09:58 +0900

    sleep時間を変更可能にした

--- main_test.go
+++ main_test.go
@@ -4,15 +4,8 @@ import (
 	"bytes"
 	"reflect"
 	"testing"
+	"time"
 )
-
-type SpySleeper struct {
-	Calls int
-}
-
-func (s *SpySleeper) Sleep() {
-	s.Calls++
-}
 
 type CountdownOperationsSpy struct {
 	Calls []string
@@ -65,4 +58,24 @@ Go!`
 			t.Errorf("wanted calls %v got %v", want, spySleepPrinter.Calls)
 		}
 	})
+}
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) Sleep(duration time.Duration) {
+	s.durationSlept = duration
+}
+
+func TestConfigurableSleeper(t *testing.T) {
+	sleepTime := 5 * time.Second
+
+	spyTime := &SpyTime{}
+	sleeper := ConfigurableSleeper{sleepTime, spyTime.Sleep}
+	sleeper.Sleep()
+
+	if spyTime.durationSlept != sleepTime {
+		t.Errorf("should have slept for %v but slept for %v", sleepTime, spyTime.durationSlept)
+	}
 }
